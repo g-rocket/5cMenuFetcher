60225e68ec78cf2c8a8a001be1fe4987be75b94c
g_rocket <g.rocket1@gmail.com>
2016-03-13 09:00:59 -0700

    Add full menus

--- src/main/java/io/yancey/menufetcher/WebpageCreator.java
+++ src/main/java/io/yancey/menufetcher/WebpageCreator.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 import org.jsoup.*;
 import org.jsoup.nodes.*;
+import org.jsoup.select.*;
 
 public class WebpageCreator {
 	
@@ -77,8 +78,107 @@ public class WebpageCreator {
 	}
 
 	private static void addFullMenus(Document template, List<Menu> menus) {
-		// TODO Auto-generated method stub
-		System.err.println("addFullMenus not implemented yet");
+		Element menuTable = template.getElementById("menu").child(0);
+		for(Menu menu: menus) {
+			addStationNames(menuTable, menu);
+		}
+		
+		List<String> mealTitles = setupMealTitles(template, menus);
+		
+		for(String mealTitle: mealTitles) {
+			addFoodForMeal(mealTitle, menus, menuTable);
+		}
+	}
+	
+	private static void addFoodForMeal(String mealTitle, List<Menu> menus, Element menuTable) {
+		for(Menu menu: menus) {
+			Meal thisMeal = null;
+			for(Meal meal: menu.meals) {
+				if(meal.name.equalsIgnoreCase(mealTitle)) {
+					thisMeal = meal;
+				}
+			}
+			if(thisMeal == null) {
+				Elements stations = menuTable.getElementsByClass("menu-row-"+menu.diningHallId);
+				if(stations.size() == 0) continue;
+				Element spacer = stations.first().appendElement("td");
+				spacer.attr("rowspan",Integer.toString(stations.size()));
+				spacer.addClass("menu-cell");
+				spacer.addClass("menu-spacer");
+			} else {
+				for(Station station: thisMeal.stations) {
+					String stationId = getStationIdFromName(station.name);
+					Element cell = menuTable.getElementById("menu-row-" + menu.diningHallId + "-" + stationId).appendElement("td");
+					cell.attr("id","menu-cell-" + menu.diningHallId + "-" + stationId + "-" + mealTitle);
+					cell.addClass("menu-cell");
+					cell.addClass(mealTitle);
+					Element list = cell.appendElement("ul").addClass("menu-item-list");
+					for(MenuItem item: station.menu) {
+						list.appendElement("li").appendText(item.toString());
+					}
+				}
+				for(Element stationElement: menuTable.getElementsByClass("menu-row-" + menu.diningHallId)) {
+					if(stationElement.getElementsByClass(mealTitle).size() == 0) {
+						Element spacer = stationElement.appendElement("td");
+						spacer.attr("id", stationElement.id() + "-" + mealTitle);
+						spacer.addClass("menu-cell");
+						spacer.addClass("menu-spacer");
+					}
+				}
+			}
+		}
+	}
+
+	private static List<String> setupMealTitles(Document template, List<Menu> menus) {
+		Set<String> unusedMealTitles = new HashSet<>(Arrays.asList("breakfast","lunch","brunch","dinner"));
+		for(Menu menu: menus) {
+			for(Meal meal: menu.meals) {
+				unusedMealTitles.remove(meal.name.toLowerCase());
+			}
+		}
+		for(String mealTitle: unusedMealTitles) {
+			template.getElementById("menu-header-meals-" + mealTitle).remove();
+		}
+		List<String> mealTitles = new ArrayList<>(Arrays.asList("breakfast","lunch","brunch","dinner"));
+		mealTitles.removeAll(unusedMealTitles);
+		return mealTitles;
+	}
+	
+	private static void addStationNames(Element menuTable, Menu menu) {
+		List<String> stationNames = new ArrayList<>();
+		for(Meal meal: menu.meals) {
+			for(Station station: meal.stations) {
+				if(!stationNames.contains(station.name)) {
+					stationNames.add(station.name);
+				}
+			}
+		}
+		boolean isFirstStation = true;
+		for(String station: stationNames) {
+			String stationId = getStationIdFromName(station);
+			Element stationRow = menuTable.appendElement("tr");
+			stationRow.attr("id","menu-row-"+menu.diningHallId+"-"+stationId);
+			stationRow.addClass("menu-row-"+menu.diningHallId);
+			if(isFirstStation) {
+				Element diningHallName = stationRow.appendElement("td");
+				diningHallName.attr("id", "menu-title-" + menu.diningHallId);
+				diningHallName.addClass("menu-cell");
+				diningHallName.addClass("menu-title");
+				diningHallName.text(menu.diningHallName);
+				diningHallName.attr("rowspan",Integer.toString(stationNames.size()));
+				isFirstStation = false;
+			}
+			Element stationName = stationRow.appendElement("td");
+			stationName.attr("id", 
+					"menu-title-" + menu.diningHallId + "-station-" + stationId);
+			stationName.addClass("menu-cell");
+			stationName.text(station);
+			
+		}
+	}
+	
+	private static String getStationIdFromName(String stationName) {
+		return stationName.toLowerCase().replaceAll("\\s+", "-");
 	}
 
 	private static void setupDayList(Document template, LocalDate day) {
